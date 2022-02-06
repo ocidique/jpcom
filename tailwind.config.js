ef34bcd3017e85f9818a6da2720e218888a92c92
Juho Pekkala <juho.pekkala@gmail.com>
2022-02-06 14:32:15 +0200

    feat: add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,md}"],
   darkMode: "class",
   theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      sans: ["Poppins", "sans-serif"],
+      mono: ["Roboto Mono", "monospace"],
+      "roboto-mono": ["Roboto Mono", "monospace"]
+    },
     extend: {},
   },
   plugins: [],
