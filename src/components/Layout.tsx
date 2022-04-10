3b4ba603baf7004ba15d85b4d2c89410a431639e
Juho Pekkala <juho.pekkala@gmail.com>
2022-04-10 20:29:42 +0300

    feat: add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navigation from "./Navigation";
+import Footer from "./Footer";
 
 type Props = {
   children?: React.ReactNode;
@@ -8,8 +9,13 @@ type Props = {
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className="min-h-screen relative dark:bg-zinc-800 z-30">
-      <Navigation />
-      {children}
+      <div className="max-w-5xl mx-auto flex flex-col min-h-screen justify-between">
+        <div className="space-y-12">
+          <Navigation />
+          {children}
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 };
