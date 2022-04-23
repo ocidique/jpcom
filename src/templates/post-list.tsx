c8951130c01394f71032ae638ebaf8359cceb451
Juho Pekkala <juho.pekkala@gmail.com>
2022-04-23 23:13:30 +0300

    feat: add pinned post capability

--- src/templates/post-list.tsx
+++ src/templates/post-list.tsx
@@ -60,10 +60,28 @@ const BlogList = ({ pageContext, data }) => {
               <Link
                 to={slug}
                 key={slug}
-                className="inline-block relative group transform hover:scale-[1.01] transition-all h-56 shadow-lg"
+                className={
+                  node.frontmatter.pin
+                    ? "inline-block relative group transform hover:scale-[1.01] transition-all h-56 shadow-lg col-span-2"
+                    : "inline-block relative group transform hover:scale-[1.01] transition-all h-56 shadow-lg col-span-1"
+                }
               >
-                <div className="absolute flex z-10 bg-black opacity-20 group-hover:opacity-10 transform transition-opacity w-full h-full rounded-xl"></div>
+                {node.frontmatter.pin && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 384 512"
+                    className="absolute text-white w-6 h-6 z-30 left-3 top-4"
+                    stroke="currentColor"
+                  >
+                    {/* Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.*/}
+                    <path
+                      fill="currentColor"
+                      d="M32 32C32 14.33 46.33 0 64 0H320C337.7 0 352 14.33 352 32C352 49.67 337.7 64 320 64H290.5L301.9 212.2C338.6 232.1 367.5 265.4 381.4 306.9L382.4 309.9C385.6 319.6 383.1 330.4 377.1 338.7C371.9 347.1 362.3 352 352 352H32C21.71 352 12.05 347.1 6.04 338.7C.0259 330.4-1.611 319.6 1.642 309.9L2.644 306.9C16.47 265.4 45.42 232.1 82.14 212.2L93.54 64H64C46.33 64 32 49.67 32 32zM224 384V480C224 497.7 209.7 512 192 512C174.3 512 160 497.7 160 480V384H224z"
+                    />
+                  </svg>
+                )}
 
+                <div className="absolute flex z-10 bg-black opacity-20 group-hover:opacity-10 transform transition-opacity w-full h-full rounded-xl"></div>
                 <div className="absolute flex flex-col justify-end w-full h-full p-4 z-20 text-zinc-100 space-y-1">
                   <h3 className="text-lg text-zinc-100">{postTitle}</h3>
 
@@ -135,7 +153,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }, limit: $limit, skip: $skip) {
+    allMdx(sort: { fields: [frontmatter___pin, frontmatter___date], order: [ASC, DESC] }, limit: $limit, skip: $skip) {
       edges {
         node {
           id
@@ -145,12 +163,13 @@ export const pageQuery = graphql`
             slug
             title
             tags
+            pin
             hero_image_alt
             hero_image_credit_link
             hero_image_credit_text
             hero_image {
               childImageSharp {
-                gatsbyImageData(width: 600, placeholder: BLURRED)
+                gatsbyImageData(width: 768, placeholder: BLURRED)
               }
             }
           }
