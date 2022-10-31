4c68c71051f81169f5383b2ffcf8e83a78d65f7d
Juho Pekkala <juho.pekkala@gmail.com>
2022-10-31 22:54:05 +0200

    chore: remove obsolote react helmet plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -21,7 +21,6 @@ const config: GatsbyConfig = {
       },
     },
     "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
